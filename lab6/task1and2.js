ab489d7657be1087815cbcb0f93f18501c2e1f50
alishadawood <alishaqureshiqureshi860@gmail.com>
2025-11-07 00:43:52 -0800

    Add files via upload

---
+++ lab6/task1and2.js
@@ -0,0 +1,46 @@
+let bigInt = 123456789101112131415161718;
+console.log(typeof bigInt); 
+let num = "2005";
+console.log(typeof num); 
+let converted = Number(num); 
+console.log(typeof converted); 
+console.log(Number("45")); 
+
+
+const Product = {
+  id: 11,
+  name: "Glass Cleaner",
+  brand: "PakWheels",
+  orgPrice: 1108,
+  inStock: true,
+  disCount:20,
+  disPrice:958
+
+};
+
+console.log(Product);
+
+
+const InstProfile = {
+  username: "alishaqureshi16",
+  followers: 92,
+  following: 103,
+  posts: 0,
+  bio: "Kashmiri soul|BSCS@Riphah,Islambad|Blooming in silence,glowing in code",
+  isVerified: false
+};
+
+console.log(InstProfile);
+
+var a = 6;
+
+console.log(a++);
+console.log(a);
+var a=9;
+console.log(++a);
+var a=10;
+console.log(a--); 
+console.log(a);
+var a=5;
+console.log(--a); 
+
---
+++ lab6/task3and4.js
@@ -0,0 +1,37 @@
+console.log("web App dev");
+a=null;
+y=undefined;
+console.log(y);
+console.log(a);
+var name="Alisha";
+console.log(name);
+var name="Arfa";
+console.log(name);
+var name="saran";
+console.log(name);
+if(true){
+    var test="inside block";
+}
+console.log(test);
+let age = 25;
+console.log(age); // Output: 25
+
+age = 30; // Updating allowed
+console.log(age); // Output: 30
+
+if (true) {
+    let insideBlock = "Hello";
+    console.log(insideBlock); // Output: Hello
+}
+//console.log(insideBlock); 
+    var time=prompt("hey whats the time:");
+    if(time>5&&time<17){
+        alert("good morning");
+
+
+
+    }
+    else{
+    alert("good morning");
+        
+    }
